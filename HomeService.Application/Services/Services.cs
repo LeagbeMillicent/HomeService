24ab96678a910cba8cef98fd5ffdcef081ce2967
Frank Addai <hacphran122@gmail.com>
2024-04-10 23:44:57 +0000

    Chages in Services Registration

--- HomeService.Application/Services/Services.cs
+++ HomeService.Application/Services/Services.cs
@@ -10,10 +10,7 @@ namespace HomeService.Application.Services
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             return services;
 
         }
