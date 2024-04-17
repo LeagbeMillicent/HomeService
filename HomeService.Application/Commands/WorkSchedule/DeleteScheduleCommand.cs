1a87f698c0f00b9c170352336990d7c83634782e
millicent.leagbe <millicent.leagbe@Innorik.com>
2024-04-17 18:34:52 +0000

    New implementation

---
+++ HomeService.Application/Commands/WorkSchedule/DeleteScheduleCommand.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using HomeService.Application.Repository;
+using HomeService.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeService.Application.Commands.WorkSchedule
+{
+    public class DeleteScheduleCommand : IRequest<Unit>
+    {
+        public int ScheduleId { get; set; }
+    }
+
+    public class DeleteScheduleCommandHandler : IRequestHandler<DeleteScheduleCommand>
+    {
+        private readonly IGenericRepository<tblWorkSchedule> _repository;
+        private readonly IMapper _mapper;
+
+        public DeleteScheduleCommandHandler(IGenericRepository<tblWorkSchedule> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteScheduleCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entityToUpdate = await _repository.GetAsync(request.ScheduleId);
+                
+
+                _mapper.Map(request, entityToUpdate);
+                await _repository.UpdateAsync(entityToUpdate);
+                return Unit.Value;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return default;
+        }
+    }
+}
---
+++ HomeService.Application/Commands/WorkSchedule/GetAllScheduleCommand.cs
@@ -0,0 +1,43 @@
+﻿
+
+using AutoMapper;
+using HomeService.Application.DTOs.WorkSchedule;
+using HomeService.Application.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeService.Application.Commands.WorkSchedule
+{
+    public class GetAllScheduleCommand :IRequest<List<GetAllScheduleDto>>
+    {
+
+    }
+
+    public class GetAllScheduleCommandHandler : IRequestHandler<GetAllScheduleCommand, List<GetAllScheduleDto>>
+    {
+        private readonly IGenericRepository<GetAllScheduleDto> _repository;
+        private readonly IMapper _mapper;
+
+        public GetAllScheduleCommandHandler(IGenericRepository<GetAllScheduleDto> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetAllScheduleDto>> Handle(GetAllScheduleCommand request, CancellationToken cancellationToken)
+        {
+            var sqlQuery = $"Select * From tblWorkSchedule";
+            var response = await _repository.GetAllAsync(sqlQuery);
+
+            if(response != null) 
+            {
+                return _mapper.Map<List<GetAllScheduleDto>>(response);
+            }
+            return null;
+        }
+    }
+}
