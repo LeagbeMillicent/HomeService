193626a150df6b125cfc3b2cfa20f0bbfdfd3bb5
osman adamu <osman.adamu@hubtel.com>
2024-04-19 09:32:08 +0000

    Sample-login

---
+++ HomeService/Extensions/ServiceCollections.cs
@@ -0,0 +1,15 @@
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using HomeService.Domain;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HomeService.API.Extensions;
+
+public static class ServiceCollections
+{
+      
+}
