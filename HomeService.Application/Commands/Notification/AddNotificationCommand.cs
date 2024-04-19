be22ab14afcd42f00112d74c869fc54303b9a1c4
millicent.leagbe <millicent.leagbe@Innorik.com>
2024-04-19 10:11:56 +0000

    New Implemetation

---
+++ HomeService.Application/Commands/Notification/AddNotificationCommand.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HomeService.Application.DTOs.NotificationDto;
+using HomeService.Application.DTOs.PayMent;
+using HomeService.Application.Repository;
+using HomeService.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeService.Application.Commands.Notification
+{
+    public class AddNotificationCommand: IRequest<BaseResponse>
+    {
+        public AddNotificationDto notDto { get; set; }
+    }
+
+    public class AddNotificationHandler : IRequestHandler<AddNotificationCommand, BaseResponse>
+    {
+        private readonly IGenericRepository<AddNotificationDto> _repository;
+        private readonly IMapper _mapper;
+
+        public AddNotificationHandler(IGenericRepository<AddNotificationDto> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse> Handle(AddNotificationCommand request, CancellationToken cancellationToken)
+        {
+            var map = _mapper.Map<AddNotificationDto>(request.notDto);
+
+            var result = await _repository.Create(map);
+            return new BaseResponse
+            {
+                Id = result,
+                Message = " Created Succesfully"
+            };
+        }
+    }
+}
---
+++ HomeService.Application/Commands/Notification/GetAllNotificationCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using HomeService.Application.DTOs.NotificationDto;
+using HomeService.Application.DTOs.PayMent;
+using HomeService.Application.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeService.Application.Commands.Notification
+{
+    public class GetAllNotificationCommand : IRequest<List<GetAllNotificationsDto>>
+    {
+    }
+
+    public class GetAllNotificationCommandHandler : IRequestHandler<GetAllNotificationCommand, List<GetAllNotificationsDto>>
+    {
+        private readonly IGenericRepository<GetAllNotificationsDto> _repository;
+        private readonly IMapper _mapper;
+
+        public GetAllNotificationCommandHandler(IGenericRepository<GetAllNotificationsDto> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetAllNotificationsDto>> Handle(GetAllNotificationCommand request, CancellationToken cancellationToken)
+        {
+            var sqlQuery = $"Select * From tblNotification";
+            var response = await _repository.GetAllAsync(sqlQuery);
+
+            if (response != null)
+            {
+                return _mapper.Map<List<GetAllNotificationsDto>>(response);
+            }
+            return null;
+        }
+    }
+}
